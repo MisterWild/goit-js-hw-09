f866a0e33b3805d0560cb9661cebe75d5b93309c
MisterWild <00proctor00@gmail.com>
2023-04-04 18:38:32 +0300

    add src23

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,7 +1,6 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import { Report } from 'notiflix/build/notiflix-report-aio';
-
 document.body.style.backgroundColor = '#ece5da';
 const TIMER_DELAY = 1000;
 let intervalId = null;
